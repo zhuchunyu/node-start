99c790a3def5626cf05b4c430d4b538a224408f7
zhuchunyu <zhuchunyu_14a@163.com>
2017-08-21 16:06:37 +0800

    update

--- koa-app.js
+++ koa-app.js
@@ -18,6 +18,28 @@ db.once('open', function() {
     console.log('connected');
 });
 
+//定义模型
+const helloSchema = mongoose.Schema({
+    name: String,
+    created:Date
+});
+
+const Hello = mongoose.model('Hello', helloSchema);
+
+//保存数据
+const silence = new Hello({ name: 'jack', created:new Date() });
+
+silence.save().then(function () {
+    console.log('saved');
+});
+
+//查询数据
+Hello.find({name:'jack'}).exec().then(function (data) {
+    console.log(data);
+}).catch(function (err) {
+    console.log(err.toString());
+});
+
 //创建redis客户端
 const redis = require("redis"),
     client = redis.createClient({host:'redis', port:6379});
